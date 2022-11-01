381b08630686c6092a4ab54729073de44810119a
jorian <10341677+jorian@users.noreply.github.com>
2022-11-01 13:56:13 +0100

    add more title

--- src/views/reservetable.rs
+++ src/views/reservetable.rs
@@ -21,9 +21,10 @@ impl View for ReserveTable {
             .find(|b| &b.currencyid.to_string() == "iJhCezBExJHvtyH3fGhNnt2NhU4Ztkf2yq")
             .unwrap();
 
-        debug!("{:?}", printer.output_size);
+        // ----------------------- VRSC-GBP ----------------------- Price ----------- Weight
+        // iJhCezBExJHvtyH3fGhNnt2NhU4Ztkf2yq                001.00000000 | 0200000.00273192
 
-        let eofp = dbg!((printer.output_size.x / 2) - (&self.reserve_name.len() / 2));
+        let eofp = dbg!(((printer.output_size.x - 26) / 2) - (&self.reserve_name.len() / 2));
         let bolp = dbg!(eofp + &self.reserve_name.len());
 
         for i in 0..(eofp - 1) {
@@ -34,9 +35,18 @@ impl View for ReserveTable {
             printer.print((eofp - 1, 0), &format!(" {} ", &self.reserve_name));
         });
 
-        for i in (bolp + 1)..printer.output_size.x {
+        for i in (bolp + 1)..(printer.output_size.x - 26).max(25) {
             printer.print((i, 0), "-");
         }
+        printer.with_color(Color::from_256colors(32).into(), |printer| {
+            printer.print((printer.output_size.x - 25, 0), "Price");
+        });
+
+        printer.print((printer.output_size.x - 20, 0), " ----------- ");
+
+        printer.with_color(Color::from_256colors(32).into(), |printer| {
+            printer.print((printer.output_size.x - 7, 0), "Weight");
+        });
 
         for (i, rc) in self.reserve_currencies.iter().enumerate() {
             printer.print((0, i + 1), &rc.currencyid.to_string());
