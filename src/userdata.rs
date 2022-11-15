1f225231594c438e857b98aacb1434695f3d7526
jorian <10341677+jorian@users.noreply.github.com>
2022-11-15 08:32:41 +0100

    refactor getting local pbaas chains

--- src/userdata.rs
+++ src/userdata.rs
@@ -1,7 +1,4 @@
-use std::{
-    fs::{DirEntry, ReadDir},
-    path::PathBuf,
-};
+use std::{fs::ReadDir, path::PathBuf};
 
 use os_info::Type as OSType;
 use tracing::{error, info};
@@ -12,7 +9,7 @@ pub struct UserData {
 
 impl UserData {
     pub fn new(testnet: bool) -> Self {
-        let mut pbaas_chains = get_local_pbaas_chains();
+        let mut pbaas_chains = local_pbaas_chains(testnet);
 
         if testnet {
             pbaas_chains.push(PBaaSChain {
@@ -38,124 +35,32 @@ pub struct PBaaSChain {
 }
 
 impl PBaaSChain {
-    pub fn new<S: Into<String>>(name: Option<S>, currencyidhex: Option<S>) -> Self {
+    pub fn new<S: Into<String>>(currencyidhex: S) -> Self {
         PBaaSChain {
-            name: name.map(|n| n.into()),
-            currencyidhex: currencyidhex.map(|c| c.into()),
+            name: None,
+            currencyidhex: Some(currencyidhex.into()),
             zmqhashblock: None,
             zmqhashtx: None,
         }
     }
 }
 
-fn functional_pbaas_chains() -> Vec<PBaaSChain> {
-    pbaas_dir_entries()
+/// Gets all the locally installed PBaaS chains.
+/// Some assumptions have been made:
+/// - the .verustest/VerusTest directory has not been edited by a user. It assumes that all the directories that are found in .verustest are
+/// PBaaS chains. No guarantees can be given about each directory being an actual PBaaS chain.
+fn local_pbaas_chains(testnet: bool) -> Vec<PBaaSChain> {
+    pbaas_dir_entries(testnet)
         .filter_map(|d| d.ok())
-        .map_while(|pbaas_dir| {
-            // pbaas_dir is the dir we need the name of to compare it later on with the .conf file
-            std::fs::read_dir(pbaas_dir.path())
-                .ok()
-                .and_then(|mut pbaas_dir_files| {
-                    pbaas_dir_files.find_map(|file_result| {
-                        file_result.ok().and_then(|file| {
-                            dbg!(&file.file_name());
-                            if file.file_name() == pbaas_dir.file_name() {
-                                Some(PBaaSChain::new(
-                                    None, // the fullyqualifiedname of this chain is not retrievable at this point.
-                                    // TODO consider using currencyidhex as leading name and add real name during runtime where there is unicode support.
-                                    PathBuf::new()
-                                        .file_stem()
-                                        .map(|name| name.to_string_lossy().into_owned()), // VRSC makes an OS safe string called currencyidhex, so this never fails.
-                                ))
-                            } else {
-                                None
-                            }
-                        })
-                    })
-                })
+        .map(|dir| {
+            let currencyidhex = dir.file_name();
+            PBaaSChain::new(currencyidhex.to_string_lossy().to_owned())
         })
         .collect()
 }
 
-fn get_local_pbaas_chains() -> Vec<PBaaSChain> {
-    let mut installed_chains = vec![];
-
-    let dir = pbaas_dir_entries();
-
-    dir.into_iter()
-        .filter_map(|d| d.ok())
-        .for_each(|pbaas_dir| {
-            for pbaas_dir_file in std::fs::read_dir(pbaas_dir.path()) {
-                // pbaas_dir_file
-                //     .filter_map(|i| {
-                //         i.ok().and_then(|file| {
-                //             if let Some(filename) = file.file_name().to_str() {
-                //                 if filename
-                //                     == format!("{}.conf", pbaas_dir.file_name().to_str().unwrap())
-                //                 {
-                //                     let pathbuf = PathBuf::from(&file.path());
-
-                //                     if pathbuf.exists() {
-                //                         // installed_chains.push(
-                //                         Some(PBaaSChain::new(
-                //                             None, // the fullyqualifiedname of this chain is not retrievable at this point.
-                //                             // TODO consider using currencyidhex as leading name and add real name during runtime where there is unicode support.
-                //                             pathbuf
-                //                                 .file_stem()
-                //                                 .map(|name| name.to_string_lossy().into_owned()), // VRSC makes an OS safe string called currencyidhex, so this never fails.
-                //                         ))
-                //                         // )
-                //                     } else {
-                //                         None
-                //                     }
-                //                 } else {
-                //                     None
-                //                 }
-                //             } else {
-                //                 None
-                //             }
-                //         })
-                //     })
-                // .collect()
-
-                for i in pbaas_dir_file.into_iter() {
-                    // go over all the contents of each folder
-                    if let Ok(direntry) = i {
-                        match direntry.file_name().to_str() {
-                            Some(filename) => {
-                                if filename
-                                    == format!(
-                                        "{}.conf",
-                                        &pbaas_dir.file_name().into_string().unwrap()
-                                    )
-                                    .as_str()
-                                {
-                                    let pathbuf = PathBuf::from(&direntry.path());
-
-                                    if pathbuf.exists() {
-                                        installed_chains.push(PBaaSChain::new(
-                                            None, // the fullyqualifiedname of this chain is not retrievable at this point.
-                                            // TODO consider using currencyidhex as leading name and add real name during runtime where there is unicode support.
-                                            pathbuf
-                                                .file_stem()
-                                                .map(|name| name.to_string_lossy().into_owned()), // VRSC makes an OS safe string called currencyidhex, so this never fails.
-                                        ));
-                                    }
-                                }
-                            }
-                            None => {} // currencyidhex will always be OS safe strings
-                        }
-                    }
-                }
-            }
-        });
-    // .collect::<Vec<PBaaSChain>>();
-
-    installed_chains
-}
-
-fn pbaas_dir_entries() -> ReadDir {
-    if let Some(verustest_path) = pbaas_dir_location(true) {
+fn pbaas_dir_entries(testnet: bool) -> ReadDir {
+    if let Some(verustest_path) = pbaas_dir_location(testnet) {
         if let Ok(dir) = verustest_path.read_dir() {
             return dir;
         } else {
@@ -210,6 +115,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        dbg!(functional_pbaas_chains());
+        dbg!(local_pbaas_chains(true));
     }
 }
