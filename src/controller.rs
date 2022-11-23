8d45ec5ab8b4c6f9ab721dd7a399a262e9ad0a1d
jorian <10341677+jorian@users.noreply.github.com>
2022-11-23 09:23:22 +0100

    refactor and cleanup

--- src/controller.rs
+++ src/controller.rs
@@ -9,6 +9,7 @@ use chrono::Local;
 use tracing::{debug, error, info};
 use vrsc_rpc::{
     bitcoin::{hashes::sha256d::Hash, Txid},
+    json::GetRawTransactionResultVerbose,
     RpcApi,
 };
 
@@ -37,7 +38,7 @@ impl Controller {
 
         let id_names = Rc::new(RwLock::new(HashMap::new()));
 
-        let pbaas_chains = gather_pbaas_chains(testnet, Rc::clone(&id_names));
+        let pbaas_chains = get_running_chains(testnet, Rc::clone(&id_names));
         let first = Rc::clone(pbaas_chains.first().unwrap());
 
         for chain in pbaas_chains.iter() {
@@ -68,7 +69,6 @@ impl Controller {
         while self.ui.step() {
             if let Some(message) = self.c_rx.try_iter().next() {
                 match message {
-                    ControllerMessage::CurrencyModeChange => {}
                     ControllerMessage::CurrencySelectionChange => {
                         info!("Filter changed");
 
@@ -91,141 +91,23 @@ impl Controller {
 
                                 let hash = Hash::from_str(&txid).unwrap();
                                 let txid = Txid::from_hash(hash);
-                                match self
+
+                                if let Ok(raw_tx) = self
                                     .active_chain
                                     .read()
                                     .unwrap()
                                     .client()
                                     .get_raw_transaction_verbose(&txid)
                                 {
-                                    Ok(raw_tx) => {
-                                        if raw_tx.confirmations.is_none() {
-                                            for vout in &raw_tx.vout {
-                                                if let Some(reserve_transfer) =
-                                                    &vout.script_pubkey.reservetransfer
-                                                {
-                                                    debug!(
-                                                        "a transfer was initiated: {}",
-                                                        raw_tx.txid
-                                                    );
-
-                                                    if let Ok(mut write) = self.id_names.write() {
-                                                        let currencyname = write
-                                                            .entry(
-                                                                reserve_transfer
-                                                                    .destinationcurrencyid
-                                                                    .to_string(),
-                                                            )
-                                                            .or_insert_with(|| {
-                                                                self.active_chain
-                                                                    .read()
-                                                                    .unwrap()
-                                                                    .client()
-                                                                    .get_currency(
-                                                                        &reserve_transfer
-                                                                            .destinationcurrencyid
-                                                                            .to_string(),
-                                                                    )
-                                                                    .unwrap()
-                                                                    .fullyqualifiedname
-                                                            })
-                                                            .clone();
-
-                                                        debug!("currencyname: {}", &currencyname);
-
-                                                        let amount_in_currency = write
-                                                            .entry(
-                                                                reserve_transfer
-                                                                    .currencyvalues
-                                                                    .keys()
-                                                                    .last()
-                                                                    .unwrap()
-                                                                    .to_string(),
-                                                            )
-                                                            .or_insert_with(|| {
-                                                                self.active_chain
-                                                                    .read()
-                                                                    .unwrap()
-                                                                    .client()
-                                                                    .get_currency(
-                                                                        &reserve_transfer
-                                                                            .destinationcurrencyid
-                                                                            .to_string(),
-                                                                    )
-                                                                    .unwrap()
-                                                                    .fullyqualifiedname
-                                                            })
-                                                            .clone();
-
-                                                        self.l_tx
-                                                        .send(LogMessage {
-                                                            time: format!(
-                                                                "{}",
-                                                                Local::now().format("%H:%M:%S")
-                                                            ),
-                                                            _type: crate::views::log::MessageType::Initiate,
-                                                            reserve: currencyname,
-                                                            amount_in_currency: amount_in_currency,
-                                                            amount_in: vout.value,
-                                                            amount_out: None,
-                                                        })
-                                                        .unwrap();
-                                                    }
-                                                }
-                                            }
-                                        }
-                                        if raw_tx.confirmations.is_some() {
-                                            for vout in &raw_tx.vout {
-                                                // let value =
-                                                //     serde_json::to_value(&vout.script_pubkey).unwrap();
-
-                                                if let Some(_crosschain_import) =
-                                                    &vout.script_pubkey.crosschainimport
-                                                {
-                                                    info!(
-                                                        "a transfer was settled: {}",
-                                                        raw_tx.txid
-                                                    );
-                                                    // info!("crosschainimport {:#?}", crosschain_import);
-
-                                                    // let currencyname = self.verus.currency_id_to_name(
-                                                    //     crosschain_import.importcurrencyid.clone(),
-                                                    // );
-
-                                                    // self.l_tx
-                                                    //     .send(LogMessage {
-                                                    //         time: format!(
-                                                    //             "{}",
-                                                    //             Local::now().format("%H:%M:%S")
-                                                    //         ),
-                                                    //         _type: crate::views::log::MessageType::Initiate,
-                                                    //         reserve: currencyname,
-                                                    //         amount_in: crosschain_import
-                                                    //             .valuein
-                                                    //             .as_f64()
-                                                    //             .unwrap(), //.as_vrsc(),
-                                                    //         amount_out: None,
-                                                    //     })
-                                                    //     .unwrap();
-                                                }
-                                                // if let Some(object) = value["reserveoutput"].as_object() {
-                                                //     info!("reserveoutput {:#?}", object);
-                                                // }
-                                            }
-                                        }
-                                    }
-                                    Err(e) => error!("{:?}", e),
+                                    process_transaction(
+                                        raw_tx,
+                                        Rc::clone(&self.id_names),
+                                        Rc::clone(&self.active_chain),
+                                        self.l_tx.clone(),
+                                    )
                                 }
                             }
                         }
-                        // 1d878bf932c406647374cafa9019ee5b00c581309e01f772d6e147f34b6bc601 = reservetransfer > spenttxid
-                        // 0b80f3f5b0932f47c6d75f67085979cf5067b60077f3196f080fa788f078804d
-                        // 6c070618db343c1ba288f7da713729540058c4e54ea63b5ac0c5757fc5166d76
-
-                        // 59f9b15870491c4112b6b892f5d3a54a8ad301503071c27a885429ce4df2a86d VRSCTEST -> VRSC-EUR
-
-                        // 1b04030001011504af02625e74df9de1cf78921e0690ab94b2d6c603cc3604030901011504af02625e74df9de1cf78921e0690ab94b2d6c6031a0176f89c6dc26d4d775b3dceef7ad4f1d3efd35a0380e9aacb0d75
-                        // 1b04030001011504af02625e74df9de1cf78921e0690ab94b2d6c603cc3604030901011504af02625e74df9de1cf78921e0690ab94b2d6c6031a0176f89c6dc26d4d775b3dceef7ad4f1d3efd35a0380e9c8bf0775
 
                         let cb_sink = self.ui.siv.cb_sink().clone();
                         cb_sink
@@ -259,6 +141,118 @@ impl Controller {
                             .send(UIMessage::PBaasDialog(c_tx, labels))
                             .unwrap();
                     }
+                }
+            }
+        }
+
+        // 1d878bf932c406647374cafa9019ee5b00c581309e01f772d6e147f34b6bc601 = reservetransfer > spenttxid
+        // 0b80f3f5b0932f47c6d75f67085979cf5067b60077f3196f080fa788f078804d
+        // 6c070618db343c1ba288f7da713729540058c4e54ea63b5ac0c5757fc5166d76
+
+        // 59f9b15870491c4112b6b892f5d3a54a8ad301503071c27a885429ce4df2a86d VRSCTEST -> VRSC-EUR
+
+        // 1b04030001011504af02625e74df9de1cf78921e0690ab94b2d6c603cc3604030901011504af02625e74df9de1cf78921e0690ab94b2d6c6031a0176f89c6dc26d4d775b3dceef7ad4f1d3efd35a0380e9aacb0d75
+        // 1b04030001011504af02625e74df9de1cf78921e0690ab94b2d6c603cc3604030901011504af02625e74df9de1cf78921e0690ab94b2d6c6031a0176f89c6dc26d4d775b3dceef7ad4f1d3efd35a0380e9c8bf0775
+        fn process_transaction(
+            raw_tx: GetRawTransactionResultVerbose,
+            id_names: IdNames,
+            active_chain: Rc<RwLock<Box<dyn Chain>>>,
+            l_tx: mpsc::Sender<LogMessage>,
+        ) {
+            if raw_tx.confirmations.is_none() {
+                for vout in &raw_tx.vout {
+                    if let Some(reserve_transfer) = &vout.script_pubkey.reservetransfer {
+                        debug!("a transfer was initiated: {}", raw_tx.txid);
+
+                        if let Ok(mut write) = id_names.write() {
+                            let currencyname = write
+                                .entry(reserve_transfer.destinationcurrencyid.to_string())
+                                .or_insert_with(|| {
+                                    active_chain
+                                        .read()
+                                        .unwrap()
+                                        .client()
+                                        .get_currency(
+                                            &reserve_transfer.destinationcurrencyid.to_string(),
+                                        )
+                                        .unwrap()
+                                        .fullyqualifiedname
+                                })
+                                .clone();
+
+                            debug!("currencyname: {}", &currencyname);
+
+                            let amount_in_currency = write
+                                .entry(
+                                    reserve_transfer
+                                        .currencyvalues
+                                        .keys()
+                                        .last()
+                                        .unwrap()
+                                        .to_string(),
+                                )
+                                .or_insert_with(|| {
+                                    active_chain
+                                        .read()
+                                        .unwrap()
+                                        .client()
+                                        .get_currency(
+                                            &reserve_transfer.destinationcurrencyid.to_string(),
+                                        )
+                                        .unwrap()
+                                        .fullyqualifiedname
+                                })
+                                .clone();
+
+                            l_tx.send(LogMessage {
+                                time: format!("{}", Local::now().format("%H:%M:%S")),
+                                _type: crate::views::log::MessageType::Initiate,
+                                reserve: currencyname,
+                                amount_in_currency: amount_in_currency,
+                                amount_in: vout.value,
+                                amount_out: None,
+                            })
+                            .unwrap();
+                        }
+                    }
+                }
+            }
+
+            if raw_tx.confirmations.is_some() {
+                for vout in &raw_tx.vout {
+                    if let Some(crosschain_import) = &vout.script_pubkey.crosschainimport {
+                        info!("a transfer was settled: {}", raw_tx.txid);
+                        info!("crosschainimport {:#?}", crosschain_import);
+                        if let Ok(mut write) = id_names.write() {
+                            let currencyname = write
+                                .entry(crosschain_import.importcurrencyid.to_string())
+                                .or_insert_with(|| {
+                                    active_chain
+                                        .read()
+                                        .unwrap()
+                                        .client()
+                                        .get_currency(
+                                            &crosschain_import.importcurrencyid.to_string(),
+                                        )
+                                        .unwrap()
+                                        .fullyqualifiedname
+                                })
+                                .clone();
+
+                            l_tx.send(LogMessage {
+                                time: format!("{}", Local::now().format("%H:%M:%S")),
+                                _type: crate::views::log::MessageType::Initiate,
+                                reserve: currencyname,
+                                amount_in_currency: String::new(),
+                                amount_in: vout.value,
+                                amount_out: None,
+                            })
+                            .unwrap();
+                        }
+                    }
+                    // if let Some(object) = value["reserveoutput"].as_object() {
+                    //     info!("reserveoutput {:#?}", object);
+                    // }
                 }
             }
         }
@@ -297,7 +291,7 @@ impl Controller {
     }
 }
 
-pub fn gather_pbaas_chains(testnet: bool, id_names: IdNames) -> Vec<Rc<RwLock<Box<dyn Chain>>>> {
+pub fn get_running_chains(testnet: bool, id_names: IdNames) -> Vec<Rc<RwLock<Box<dyn Chain>>>> {
     let mut all_chains: Vec<Rc<RwLock<Box<dyn Chain>>>> = vec![];
 
     let v_chain: Rc<RwLock<Box<dyn Chain>>> = Rc::new(RwLock::new(Box::new(VerusChain::new(
@@ -313,13 +307,15 @@ pub fn gather_pbaas_chains(testnet: bool, id_names: IdNames) -> Vec<Rc<RwLock<Bo
     });
 
     all_chains
+        .into_iter()
+        .filter(|chain| chain.read().unwrap().client().ping().is_ok())
+        .collect()
 }
 
 pub enum ControllerMessage {
     NewBlock(String, String),
     NewTransaction(String, String),
     CurrencySelectionChange,
-    CurrencyModeChange,
     ChainChange(String),
     PBaaSDialog(mpsc::Sender<ControllerMessage>),
 }
